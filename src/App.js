e9b9b95d77b6ca59f5c0e9153efe855cd27ad0a3
rahulramagiri <rahulramagiri8@gmail.com>
2021-10-03 22:36:05 +0530

    try for the day

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
